e7b8653f0977f7e2f7f1ddf43e75fd2972b8ded1
Eldar Azamatov <azamatov3333@gmail.com>
2024-07-19 15:05:05 +0600

    Add AssetPictureComponent and asset library content
    Introduced a new component, AssetPictureComponent, for displaying asset images with associated actions. Also added asset library content and related assets handling functionalities. These changes enhance visualization and interaction capabilities within the docs/assets section.

---
+++ src/content/docs/assets.ts
@@ -0,0 +1,234 @@
+export const assets: Asset[] = [
+    {
+        type: 'terrain',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/c3krtxqyrnt',
+        buttonText: 'Get terrain',
+        altText: 'Terrain Asset',
+        json: `{
+            "id": "",
+            "name": "Untitled terrain",
+            "tileSize": 16,
+            "terrainSrc": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/c3krtxqyrnt",
+            "terrainMode": "basicborder",
+            "autoTilesetSrc": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/0bky3rvh0qcq"
+        }`
+    },
+    {
+        type: 'terrain',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/ui4jakads2',
+        buttonText: 'Get terrain',
+        altText: 'Terrain Asset',
+        json: `{
+            "id": "",
+            "name": "Untitled terrain",
+            "tileSize": 16,
+            "terrainSrc": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/ui4jakads2",
+            "terrainMode": "basicborder",
+            "autoTilesetSrc": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/36ml3b908ua"
+        }`
+    },
+    {
+        type: 'terrain',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/77n1hm4ul7n',
+        buttonText: 'Get terrain',
+        altText: 'Terrain Asset',
+        json: `{
+            "id": "",
+            "name": "Untitled terrain",
+            "tileSize": 16,
+            "terrainSrc": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/77n1hm4ul7n",
+            "terrainMode": "wang",
+            "autoTilesetSrc": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/qx21jkmwv3m"
+        }`
+    },
+    {
+        type: 'terrain',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/d9bkb58mfv',
+        buttonText: 'Get terrain',
+        altText: 'Terrain Asset',
+        json: `{
+            "id": "",
+            "name": "Untitled terrain",
+            "tileSize": 16,
+            "terrainSrc": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/d9bkb58mfv",
+            "terrainMode": "wang",
+            "autoTilesetSrc": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/mmlz50y7fv"
+        }`
+    },
+    {
+        type: 'terrain',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/n8rbeaay6hb',
+        buttonText: 'Get terrain',
+        altText: 'Terrain Asset',
+        json: `{
+            "id": "",
+            "name": "Untitled terrain",
+            "tileSize": 16,
+            "terrainSrc": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/n8rbeaay6hb",
+            "terrainMode": "wang",
+            "autoTilesetSrc": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/3lhfvpc6orr"
+        }`
+    },
+    {
+        type: 'terrain',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/5i2ge69xtuf',
+        buttonText: 'Get terrain',
+        altText: 'Terrain Asset',
+        json: `{
+            "id": "",
+            "name": "Untitled terrain",
+            "tileSize": 16,
+            "terrainSrc": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/5i2ge69xtuf",
+            "terrainMode": "wang",
+            "autoTilesetSrc": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/8hw0nuyq2fb"
+        }`
+    },
+    {
+        type: 'terrain',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/rah623dzec',
+        buttonText: 'Get terrain',
+        altText: 'Terrain Asset',
+        json: `{
+            "id": "",
+            "name": "Untitled terrain",
+            "tileSize": 16,
+            "terrainSrc": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/rah623dzec",
+            "terrainMode": "wang",
+            "autoTilesetSrc": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/y4uj0mtld4p"
+        }`
+    },
+    {
+        type: 'terrain',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/9lfk634b6bo',
+        buttonText: 'Get terrain',
+        altText: 'Terrain Asset',
+        json: `{
+            "id": "",
+            "name": "Untitled terrain",
+            "tileSize": 16,
+            "terrainSrc": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/9lfk634b6bo",
+            "terrainMode": "wang",
+            "autoTilesetSrc": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/c8qslxawkyl"
+        }`
+    },
+    {
+        type: 'mapObject',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/8xslu5ubwqi-bench.png',
+        buttonText: 'Get object',
+        altText: 'Map Object Asset',
+        json: `{
+            "id": "",
+            "src": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/8xslu5ubwqi-bench.png",
+            "name": "Untitled object",
+            "depthOrigin": 0
+        }`
+    },
+    {
+        type: 'mapObject',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/ohn5tuxancr-bush.png',
+        buttonText: 'Get object',
+        altText: 'Map Object Asset',
+        json: `{
+            "id": "",
+            "src": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/ohn5tuxancr-bush.png",
+            "name": "Untitled object",
+            "depthOrigin": 0
+        }`
+    },
+    {
+        type: 'mapObject',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/uxds8eiynxq-chest.png',
+        buttonText: 'Get object',
+        altText: 'Map Object Asset',
+        json: `{
+            "id": "",
+            "src": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/uxds8eiynxq-chest.png",
+            "name": "Untitled object",
+            "depthOrigin": 0
+        }`
+    },
+    {
+        type: 'mapObject',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/l6fn0s41tkk-flower-1.png',
+        buttonText: 'Get object',
+        altText: 'Map Object Asset',
+        json: `{
+            "id": "",
+            "src": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/l6fn0s41tkk-flower-1.png",
+            "name": "Untitled object",
+            "depthOrigin": 0
+        }`
+    },
+    {
+        type: 'mapObject',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/kz6ivbk1s7-flower-2.png',
+        buttonText: 'Get object',
+        altText: 'Map Object Asset',
+        json: `{
+            "id": "",
+            "src": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/kz6ivbk1s7-flower-2.png",
+            "name": "Untitled object",
+            "depthOrigin": 0
+        }`
+    },
+    {
+        type: 'mapObject',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/oqmki0s1p6q-rock.png',
+        buttonText: 'Get object',
+        altText: 'Map Object Asset',
+        json: `{
+            "id": "",
+            "src": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/oqmki0s1p6q-rock.png",
+            "name": "Untitled object",
+            "depthOrigin": 0
+        }`
+    },
+    {
+        type: 'mapObject',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/mgba8n3ehkq-tree-1.png',
+        buttonText: 'Get object',
+        altText: 'Map Object Asset',
+        json: `{
+            "id": "",
+            "src": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/mgba8n3ehkq-tree-1.png",
+            "name": "Untitled object",
+            "depthOrigin": 0
+        }`
+    },
+    {
+        type: 'mapObject',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/x5zj0to3cuh-tree-2.png',
+        buttonText: 'Get object',
+        altText: 'Map Object Asset',
+        json: `{
+            "id": "",
+            "src": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/x5zj0to3cuh-tree-2.png",
+            "name": "Untitled object",
+            "depthOrigin": 0
+        }`
+    },
+    {
+        type: 'mapObject',
+        imagePath: 'https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/btmw5v86s6w-tree-3.png',
+        buttonText: 'Get object',
+        altText: 'Map Object Asset',
+        json: `{
+            "id": "",
+            "src": "https://api.pixelstories.io/storage/v1/object/public/story-game-assets/573fc3be-4a7b-479a-825b-fb19aaf068d3/ZL7ZJbMG9D/btmw5v86s6w-tree-3.png",
+            "name": "Untitled object",
+            "depthOrigin": 0
+        }`
+    }
+];
+
+export function handleClick(json: string | undefined): void {
+    if (json) {
+        navigator.clipboard.writeText(json).then(() => {
+            alert('JSON copied to clipboard');
+        }).catch(err => {
+            console.error('Failed to copy: ', err);
+        });
+    } else {
+        alert('No JSON data to copy');
+    }
+}
